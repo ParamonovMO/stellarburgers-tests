19994976b12b440e9ec0114c601b017008b0d005
ParamonovMO <paramonov.maxim@vk.com>
2026-07-20 23:13:03 +0500

    Скрыты данные пользователя через dotend и добавлены секреты в CI

--- helpers/user_data.py
+++ helpers/user_data.py
@@ -1,11 +1,33 @@
-class PersonalData:
-    """Корректные данные для авторизации"""
+import os
+from pathlib import Path
 
-    USER_EMAIL = "paramonov.maxim@vk.com"  # Электронная почта
-    USER_PASSWORD = "13542Qwe"             # Пароль
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+ENV_PATH = BASE_DIR / '.env'
+
+load_dotenv(ENV_PATH)
+
+
+class PersonalData:
+    """Корректные данные для авторизации загружаются из .env"""
+
+    USER_EMAIL = os.getenv('USER_EMAIL')            # Электронная почта
+    USER_PASSWORD = os.getenv('USER_PASSWORD')      # Пароль
+
+    @classmethod
+    def validate(cls) -> None:
+        "Проверка, что данные пользователя загружены. Если None, то тесты не стартуют."
+        if not cls.USER_EMAIL or not cls.USER_PASSWORD:
+            raise EnvironmentError("Константы логина и пароля не найдены в файле .env"
+                                   )
+
 
 class IncorrectPersonalData:
     """Некорректные данные для авторизации"""
 
-    USER_EMAIL = "test"                    # Электронная почта
-    USER_PASSWORD = "test"                 # Пароль
+    USER_EMAIL = "test"                             # Электронная почта
+    USER_PASSWORD = "test"                          # Пароль
+
+
+PersonalData.validate()
